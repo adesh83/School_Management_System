6046b872646c1a69fc8145ea09465a4712e92d7a
adesh83 <159472512+adesh83@users.noreply.github.com>
2024-12-03 00:16:42 +0530

    Add files via upload

---
+++ Enrollment.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Data.SqlClient;
+
+namespace Schoolmanagementsystem
+{
+    public partial class Enrollment : Form
+    {
+        public Enrollment()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker2.CustomFormat = "dd/MM/yyyy";
+        }
+
+        private void dateTimePicker2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                dateTimePicker2.CustomFormat = "";
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source= ADESHMULIK7780\SQLSERVER2017;Initial Catalog=schooldb;Integrated Security=True;Encrypt=False");
+            con.Open();
+
+            SqlCommand cnn = new SqlCommand("insert into enrollmenttab values (@eid,@studentname,@section,@enrolldate)", con);
+            cnn.Parameters.AddWithValue("@EId", int.Parse(textBox1.Text));
+            cnn.Parameters.AddWithValue("@StudentName", textBox2.Text);
+            cnn.Parameters.AddWithValue("@Section", textBox3.Text);
+            cnn.Parameters.AddWithValue("@EnrollDate", dateTimePicker2.Value);
+            
+            
+            cnn.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Record saved Successfully", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source= ADESHMULIK7780\SQLSERVER2017;Initial Catalog=schooldb;Integrated Security=True;Encrypt=False");
+            con.Open();
+
+            SqlCommand cnn = new SqlCommand("Select * from enrollmenttab", con);
+            SqlDataAdapter da = new SqlDataAdapter(cnn);
+            DataTable table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source= ADESHMULIK7780\SQLSERVER2017;Initial Catalog=schooldb;Integrated Security=True;Encrypt=False");
+            con.Open();
+
+            SqlCommand cnn = new SqlCommand("update enrollmenttab set studentname=@studentname,section=@section,enrolldate=@enrolldate where eid=@eid", con);
+            cnn.Parameters.AddWithValue("@EId", int.Parse(textBox1.Text));
+            cnn.Parameters.AddWithValue("@StudentName", textBox2.Text);
+            cnn.Parameters.AddWithValue("@Section", textBox3.Text);
+            cnn.Parameters.AddWithValue("@EnrollDate", dateTimePicker2.Value);
+           
+            
+            cnn.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Record Updated Successfully", "update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source= ADESHMULIK7780\SQLSERVER2017;Initial Catalog=schooldb;Integrated Security=True;Encrypt=False");
+            con.Open();
+
+            SqlCommand cnn = new SqlCommand("delete enrollmenttab where eid=@eid", con);
+            cnn.Parameters.AddWithValue("@EId", int.Parse(textBox1.Text));
+
+            cnn.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Record deleted Successfully", "deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source= ADESHMULIK7780\SQLSERVER2017;Initial Catalog=schooldb;Integrated Security=True;Encrypt=False");
+            con.Open();
+
+            SqlCommand cnn = new SqlCommand("Select * from enrollmenttab", con);
+            SqlDataAdapter da = new SqlDataAdapter(cnn);
+            DataTable table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        private void Enrollment_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source= ADESHMULIK7780\SQLSERVER2017;Initial Catalog=schooldb;Integrated Security=True;Encrypt=False");
+            con.Open();
+
+            SqlCommand cnn = new SqlCommand("Select * from enrollmenttab", con);
+            SqlDataAdapter da = new SqlDataAdapter(cnn);
+            DataTable table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+    }
+}
