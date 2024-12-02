f2183c99a7429433e47a1f8208a694134063ced6
adesh83 <159472512+adesh83@users.noreply.github.com>
2024-12-03 00:11:39 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Schoolmanagementsystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click_1(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(@"Data Source= ADESHMULIK7780\SQLSERVER2017;Initial Catalog=schooldb;Integrated Security=True;Encrypt=False");
+            con.Open();
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            SqlCommand cnn = new SqlCommand("select Username, Password from logintab where Username='" + txtUsername.Text + "' and Password='" + txtPassword.Text + "'", con);
+            SqlDataAdapter da = new SqlDataAdapter(cnn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                Main mn = new Main();
+                mn.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or Password");
+            }
+            con.Close();
+        }
+    }
+}
