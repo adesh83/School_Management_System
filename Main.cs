00989502c89fa7462dab023f44abc61c1e69e543
adesh83 <159472512+adesh83@users.noreply.github.com>
2024-12-03 00:12:14 +0530

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Schoolmanagementsystem
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student st = new Student();
+            st.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Subject sj = new Subject();
+            sj.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Teacher tr=new Teacher();
+            tr.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Section se=new Section();
+            se.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Enrollment en=new Enrollment();
+            en.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Attendence att=new Attendence();
+            att.Show();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Dashboard da=new Dashboard();
+            da.Show();
+        }
+    }
+}
